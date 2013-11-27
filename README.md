c19c0de5ebea7f7a7b871e52f96e8001452b5f34
Sajjad Zaidi <zaidi11c@gmail.com>
2013-11-27 14:53:12 -0500

    First commit and README update

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ztodo
+
+Simple to-do app for my capstone project
+
+Made with my mentor at [Bloc](http://bloc.io)
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ztodo::Application.config.secret_key_base = '080f30befe7efa76c93f9b7bf9431b84e8b9f9a94ecf4782984df0ca8199243f45b8f8bfccf568cd69e068d2414d5da4f44012cfbfda0dca56d9f2254115d4a8'
