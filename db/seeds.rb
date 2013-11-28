33ab15140d488a964e6901cfbe63fb5037f988c2
Sajjad Zaidi <zaidi11c@gmail.com>
2013-11-27 21:09:38 -0500

    Created html and db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create 15 task
+Task.create(title: "Test0")
+Task.create(title: "Test1")
+Task.create(title: "Test2")
+Task.create(title: "Test3")
